27891d7410a92868d14b27a3b24321db724141fc
Muhamad-Nady <muhamednady20@gmail.com>
2023-02-04 15:10:50 +0200

    initiol commit

---
+++ source_code_AI_Model/pascal_voc_2_Yolo_format.py
@@ -0,0 +1,22 @@
+def convert(size, box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = (box[0] + box[1])/2.0
+    y = (box[2] + box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
+im=Image.open(img_path)
+w= int(im.size[0])
+h= int(im.size[1])
+
+
+print(xmin, xmax, ymin, ymax) #define your x,y coordinates
+b = (xmin, xmax, ymin, ymax)
+bb = convert((w,h), b)
+
